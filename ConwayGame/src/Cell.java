7e5456a71a5df5230db46ab503a82c3ce187d2db
huangdant@ecs.vuw.ac.nz <akistar518@gmail.com>
2018-08-20 14:06:36 +1200

    cell and grid class init

---
+++ ConwayGame/src/Cell.java
@@ -0,0 +1,12 @@
+
+public class Cell {
+    private int x;// display the x position
+    private int y;// display the y position
+    private boolean isAlive;
+    private Cell[][] neighbours;// store the neighbours
+    
+	public Cell() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ConwayGame/src/Grid.java
@@ -0,0 +1,11 @@
+
+public class Grid {
+    private int height;
+    private int width;
+    private Cell[][] cells;
+    
+	public Grid() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
