833cb7aa2be38c75c54c3e286e0f4dde167f6502
jonesgare1 <jonesgare1@gs160b.AD.ECS.VUW.AC.NZ>
2018-08-20 16:41:12 +1200

    Add method stubs to Grid

--- ConwayGame/src/Grid.java
+++ ConwayGame/src/Grid.java
@@ -15,5 +15,34 @@ public class Grid {
 		this.width = width;
 		cells = new Cell[height][width];
 	}
+	
+	/**
+	 * Fills the whole grid with cells, a random fix of alive and dead.
+	 * 
+	 * Should call provideNeighbours with each Cell as an argument after creating all the cells.
+	 */
+	public void randomFill() {
+		//TODO: Implement
+	}
+	
+	/**
+	 * Applies the rules of Conway's Game of Life to update the grid.
+	 * 
+	 * Should call prepareForUpdate() on each Cell, then call update() on each Cell.
+	 */
+	public void update() {
+		//TODO: Implement
+	}
+	
+	/**
+	 * Finds all the adjacent Cells to a given Cell and stores them.
+	 * 
+	 * Stores them using c.setNeighbours.
+	 * 
+	 * @param c The Cell that needs its neighbours provided.
+	 */
+	public void provideNeighbours(Cell c) {
+		//TODO: Implement
+	}
 
 }
