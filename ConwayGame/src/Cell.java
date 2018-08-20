a701e5fb8f8d6f133a95af43aed9b81a1c64a93c
jonesgare1 <jonesgare1@gs160b.AD.ECS.VUW.AC.NZ>
2018-08-20 15:11:22 +1200

    Add comments to Cell

--- ConwayGame/src/Cell.java
+++ ConwayGame/src/Cell.java
@@ -1,6 +1,11 @@
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A Cell object represents a single cell in the grid of Conway's Game of Life.
+ * It keeps track of its neighbours and whether it is alive or dead.
+ * @author jonesgare1
+ *
+ */
 public class Cell {
     private int x;// display the x position
     private int y;// display the y position
@@ -11,6 +16,37 @@ public class Cell {
 		this.x = x;
 		this.y = y;
 		this.isAlive = isAlive;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public List<Cell> getNeighbours() {
+		return neighbours;
+	}
+	
+	/**
+	 * Sets the neighbours of this cell.
+	 * 
+	 * Should be run once by the Grid class, once all the cells have been created.
+	 * 
+	 * @param neighbours The up-to-8 Cells adjacent to this cell
+	 */
+	public void setNeighbours(List<Cell> neighbours) {
+		this.neighbours = neighbours;
 	}
 
 }
