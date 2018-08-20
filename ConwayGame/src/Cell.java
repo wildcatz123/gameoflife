790f2cea2b313e7c821f218d34eeb6467602f79c
jonesgare1 <jonesgare1@gs160b.AD.ECS.VUW.AC.NZ>
2018-08-20 14:59:27 +1200

    Constructor for Cell

--- ConwayGame/src/Cell.java
+++ ConwayGame/src/Cell.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cell {
     private int x;// display the x position
     private int y;// display the y position
     private boolean isAlive;
-    private Cell[][] neighbours;// store the neighbours
+    private List<Cell> neighbours;// store the neighbours
     
-	public Cell() {
-		// TODO Auto-generated constructor stub
+	public Cell(int x, int y, boolean isAlive) {
+		this.x = x;
+		this.y = y;
+		this.isAlive = isAlive;
 	}
 
 }
