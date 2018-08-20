17da43e99b25047ce6040d1570a8616468f2b9d8
jonesgare1 <jonesgare1@gs160b.AD.ECS.VUW.AC.NZ>
2018-08-20 13:25:29 +1200

    Made Game extend Application

--- ConwayGame/src/Game.java
+++ ConwayGame/src/Game.java
@@ -1,8 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Game {
+public class Game extends Application {
 
 	public Game() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
