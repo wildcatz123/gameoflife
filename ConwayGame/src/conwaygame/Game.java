e89b2c42ea42cb06ec08a7de007f0e5d4e2f5ed6
chadz <chadz@10.140.88.135>
2018-08-21 16:44:02 +1200

    day 2 UI addaed speed/colour/shapw with menu button add new menu button class to UI

--- ConwayGame/src/conwaygame/Game.java
+++ ConwayGame/src/conwaygame/Game.java
@@ -1,6 +1,5 @@
 package conwaygame;
 
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -9,6 +8,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Separator;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -20,9 +21,10 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Game extends Application {
-	private int width = 500; // scene width
-	private int height = 500 + 100;// scene height
-	private Grid grids = new Grid(500, 500);
+	/*
+	 * private int width = 500; // scene width private int height = 500 + 100;//
+	 * scene height
+	 */ private Grid grids = new Grid(500, 500);
 
 	public Game() {
 		// TODO Auto-generated constructor stub
@@ -33,54 +35,68 @@ public class Game extends Application {
 	 * the stage of the game
 	 */
 	public void start(Stage primaryStage) throws Exception {
-		
-		//Initialize  the game
-		
-		
-		
-		
-		
-		
-		
-		
+
+		// Initialize the game
+
 		grids.initialFill();
 
-		
-		
-		
-		
 		// basic UI button creation
 		Button clear = new Button("Clear");
 		clear.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
 		clear.setOnMouseEntered(new ButtonColorHandler(clear, Color.MEDIUMSEAGREEN));
 		clear.setOnMouseExited(new ButtonColorHandler(clear, Color.KHAKI));
-		
+
 		Button play = new Button("Play");
 		play.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
 		play.setOnMouseEntered(new ButtonColorHandler(play, Color.MEDIUMSEAGREEN));
 		play.setOnMouseExited(new ButtonColorHandler(play, Color.KHAKI));
-		
+
 		Button randomFill = new Button("Random Fill");
 		randomFill.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
 		randomFill.setOnMouseEntered(new ButtonColorHandler(randomFill, Color.MEDIUMSEAGREEN));
 		randomFill.setOnMouseExited(new ButtonColorHandler(randomFill, Color.KHAKI));
-		
+
 		Button stop = new Button("Stop");
 		stop.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
 		stop.setOnMouseEntered(new ButtonColorHandler(stop, Color.MEDIUMSEAGREEN));
 		stop.setOnMouseExited(new ButtonColorHandler(stop, Color.KHAKI));
-		
+
 		Button exit = new Button("Exit");
 		exit.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
 		exit.setOnMouseEntered(new ButtonColorHandler(exit, Color.MEDIUMSEAGREEN));
 		exit.setOnMouseExited(new ButtonColorHandler(exit, Color.KHAKI));
+
+		MenuItem shape1 = new MenuItem("Shape 1");
+		MenuItem shape2 = new MenuItem("shape 2");
+		MenuItem shape3 = new MenuItem("Shape 3");
+		MenuButton menuButton = new MenuButton("Shape Choose", null, shape1, shape2, shape3);
+		menuButton.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
+		menuButton.setOnMouseEntered(new MenuButtonColorHandler(menuButton, Color.MEDIUMSEAGREEN));
+		menuButton.setOnMouseExited(new MenuButtonColorHandler(menuButton, Color.KHAKI));
+
+		MenuItem slowSpeed = new MenuItem("Slow");
+		MenuItem medSpeed = new MenuItem("Medium");
+		MenuItem fastSpeed = new MenuItem("Fast");
+		MenuButton speedButton = new MenuButton("Speed", null, slowSpeed, medSpeed, fastSpeed);
+		speedButton.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
+		speedButton.setOnMouseEntered(new MenuButtonColorHandler(speedButton, Color.MEDIUMSEAGREEN));
+		speedButton.setOnMouseExited(new MenuButtonColorHandler(speedButton, Color.KHAKI));
+
+		MenuItem color1 = new MenuItem("color1");
+		MenuItem color2 = new MenuItem("color1");
+		MenuItem color3 = new MenuItem("Color2");
+		MenuItem color4 = new MenuItem("Colour Picker");
+		MenuButton colorButton = new MenuButton("Clour", null, color1, color2, color3, color4);
+		colorButton.setBackground(new Background(new BackgroundFill(Color.KHAKI, null, null)));
+		colorButton.setOnMouseEntered(new MenuButtonColorHandler(colorButton, Color.MEDIUMSEAGREEN));
+		colorButton.setOnMouseExited(new MenuButtonColorHandler(colorButton, Color.KHAKI));
 
 		// the clear button Handler
 		clear.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent arg0) {
-				 clearButton();
+				clearButton();
 				System.out.println("Clear Button press");
 			}
 		});
@@ -101,7 +117,7 @@ public class Game extends Application {
 			@Override
 			public void handle(ActionEvent arg0) {
 				grids.randomFill();
-				System.out.println("Start Button press");
+				System.out.println("Random Fill Button press");
 			}
 		});
 
@@ -114,8 +130,7 @@ public class Game extends Application {
 				System.out.println("Stop Button press");
 			}
 		});
-
-		//// the Exit button Handler
+		// the Exit button Handler
 		exit.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
@@ -126,22 +141,146 @@ public class Game extends Application {
 			}
 		});
 
+		// ____________Chooser button Handler______
+
+		menuButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				System.out.println("Choose Button press");
+			}
+		});
+
+		slowSpeed.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Shape 1 selected");
+			}
+		});
+
+		medSpeed.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("shape2  selected");
+			}
+		});
+
+		fastSpeed.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("shape 3 selected(God Damn!! stop)");
+			}
+		});
+
+		// _______________Speed Button Hnadler______
+
+		menuButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				System.out.println("Speed Button press");
+			}
+		});
+
+		shape1.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("speed 1 selected");
+			}
+		});
+
+		shape2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("speed 2  selected");
+			}
+		});
+
+		shape3.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("speed 3 selected(Oii! Slow down)");
+			}
+		});
+		
+		
+		// _______________Colour Button Hnadler______
+
+				colorButton.setOnAction(new EventHandler<ActionEvent>() {
+
+					@Override
+					public void handle(ActionEvent arg0) {
+
+						System.out.println("Colour Button  press");
+					}
+				});
+
+				color1.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						System.out.println("Colour 1  selected");
+					}
+				});
+
+				color2.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						System.out.println("Colour 2  selected");
+					}
+				});
+
+				color3.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						System.out.println("Colour 3 selected(God Damn!! stop)");
+					}
+				});
+				
+				color3.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						System.out.println("Colour 4 selected(eww Picky Basterd!)");
+					}
+				});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		// ____________________Colour Button Handler
+
 		// stage > scene > VBox
 		// VBox contains GridPane for display the grid and HBox to display the
 		// control panel.
-
 		primaryStage.setTitle("Conway Game!!");
 		BorderPane root = new BorderPane();
 		Separator sep = new Separator();
-		HBox controlPanel = new HBox(10, play, randomFill, stop, clear, exit);
-		VBox hboxHolder = new VBox(sep,controlPanel);
-		root.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
+		HBox controlPanel = new HBox(10, menuButton, randomFill, play, stop, clear, exit);
+		HBox controlPane2 = new HBox(10, speedButton, colorButton, exit);
+		VBox hboxHolder = new VBox(sep, controlPanel, controlPane2);
 		controlPanel.setPadding(new Insets(10));
 		controlPanel.setAlignment(Pos.CENTER);
+		controlPane2.setPadding(new Insets(10));
+		controlPane2.setAlignment(Pos.CENTER);
+		root.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
 		root.setCenter(grids);
-		//grids.setGridLinesVisible(true);
 		root.setBottom(hboxHolder);
-		Scene scene = new Scene(root, 600, 600);
+		Scene scene = new Scene(root, 550, 700);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
@@ -151,22 +290,12 @@ public class Game extends Application {
 		launch(args);
 	}
 
-//all the methods
-	
-	/*clearButton();
-	playButton()
-	randomFillButton();
-	stopButton();*/
-	
-	
-	public void clearButton(){
-	//grids.getChildren().clear();
+	// all the methods
+
+	public void clearButton() {
+		// grids.getChildren().clear();
 	}
-	
-	
-	
-	
-	
+
 	/**
 	 * This ButtonColorHandler's event handler act on every button by changing
 	 * different colors
@@ -185,6 +314,29 @@ public class Game extends Application {
 		@Override
 		public void handle(MouseEvent event) {
 			btn.setBackground(new Background(new BackgroundFill(color, null, null)));
+
+		}
+
+	}
+
+	/**
+	 * This MenuButtonColorHandler's event handler act on every button by
+	 * changing different colors
+	 * 
+	 * @author chadz
+	 */
+	class MenuButtonColorHandler implements EventHandler<MouseEvent> {
+		private MenuButton btn2;
+		private Color color2;
+
+		public MenuButtonColorHandler(MenuButton btn2, Color color2) {
+			this.btn2 = btn2;
+			this.color2 = color2;
+		}
+
+		@Override
+		public void handle(MouseEvent event) {
+			btn2.setBackground(new Background(new BackgroundFill(color2, null, null)));
 
 		}
 
